584207e4c2aa1d238b2cf2787608dfd7a6b30e3b
Han Hossain <ahanuf12@gmail.com>
2020-06-29 20:37:12 -0700

    parse direction properly

--- rust/src/day03.rs
+++ rust/src/day03.rs
@@ -14,13 +14,13 @@ fn read_file() {
     let s = r#"R75,D30,R83,U83,L12,D49,R71,U7,L72
 U62,R66,U55,R34,D71,R55,D58,R83"#;
 
-    // s.split(",").flat_map(|x| x.parse::<i32>())
-    // let a: Vec<&str> = s.lines().split(",").collect();
+    let mut a = Vec::new();
     for line in s.lines() {
-        println!("{:?}", line);
-        println!("{:?}", line.split(",").collect::<Vec<&str>>());
+        let directions = line.split(",").map(|x| Direction::parse(x)).collect::<Vec<Direction>>();
+        a.push(directions);
     }
-    Direction::parse("R32");
+
+    println!("{:#?}", a);
 }
 
 #[derive(Debug)]
@@ -32,8 +32,14 @@ enum Direction {
 }
 
 impl Direction {
-    fn parse(s: &str) {
-        println!("{}", &s[..1]);
+    fn parse(s: &str) -> Self {
+        match (&s[..1], &s[1..]) {
+            ("U", x) => Direction::Up(x.parse::<usize>().unwrap()),
+            ("D", x) => Direction::Down(x.parse::<usize>().unwrap()),
+            ("L", x) => Direction::Left(x.parse::<usize>().unwrap()),
+            ("R", x) => Direction::Right(x.parse::<usize>().unwrap()),
+            _ => unreachable!()
+        }
     }
 }
 
